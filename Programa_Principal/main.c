1f3bed1beaa6ff407cb2370dde4e27fa6c778fb5
luccateodorovaz-beep <267044064+luccateodorovaz-beep@users.noreply.github.com>
2026-09-14 10:45:42 -0300

    Acabei a implementação da main. Agora vou revisar o código

--- Programa_Principal/main.c
+++ Programa_Principal/main.c
@@ -46,7 +46,7 @@ int main() {
             quantidade_listas++;
         }
 
-        else if (compara_string(comando, "SOMA") == 0){
+        else if (strcmp(comando, "SOMA") == 0){
             char nome[2], nome_2[2], nome_resultado[2];
             scanf("%s %s %s", nome, nome_2, nome_resultado);
             Lista *lista1= encontra_listas(listas, quantidade_listas, nome[0]);
@@ -59,7 +59,7 @@ int main() {
             listas[quantidade_listas] = resultado;
             quantidade_listas++;
         }
-        else if (compara_string(comando, "COEF")==0){
+        else if (strcmp(comando, "COEF")==0){
             char nome_lista[2];
             long long coeficiente;
             scanf("%s %lld", nome_lista, &coeficiente);
@@ -71,7 +71,7 @@ int main() {
             long long resultado = busca_coeficiente(lista, coeficiente);
             printf("%lld\n", resultado);
         }
-        else if (compara_string(comando, "REMOVE")==0){
+        else if (strcmp(comando, "REMOVE")==0){
             int grau;
             char nome[2];
             scanf("%s %d", nome, &grau);
@@ -83,7 +83,7 @@ int main() {
             remove_elemento(lista, grau);
             
         }
-        else if (compara_string(comando, "REMOVEMENOR")==0){
+        else if (strcmp(comando, "REMOVEMENOR")==0){
             char nome[2];
             scanf("%s", nome);
             Lista *lista = encontra_listas(listas, quantidade_listas, nome[0]);
@@ -93,7 +93,7 @@ int main() {
             }
             removemenor(lista);
         }
-        else if (compara_string(comando, "GRAU")==0){
+        else if (strcmp(comando, "GRAU")==0){
             char nome[2];
             scanf("%s", nome);
             Lista *lista = encontra_listas(listas, quantidade_listas, nome[0]);
@@ -103,7 +103,7 @@ int main() {
             }
             printf("%lld\n", grau(lista));
         }
-        else if (compara_string(comando, "IMPRIME")==0){
+        else if (strcmp(comando, "IMPRIME")==0){
             char nome[2];
             scanf("%s", nome);
             Lista *lista = encontra_listas(listas, quantidade_listas, nome[0]);
@@ -114,7 +114,7 @@ int main() {
             imprime(lista);
             printf("\n");
         }
-        else if (compara_string(comando, "IMPRIMEINV")==0){
+        else if (strcmp(comando, "IMPRIMEINV")==0){
             char nome[2];
             scanf("%s", nome);
             Lista *lista = encontra_listas(listas, quantidade_listas, nome[0]);
@@ -125,7 +125,7 @@ int main() {
             imprime_inv(lista);
             printf("\n");
         }
-        else if (compara_string(comando, "LIBERA")==0){
+        else if (strcmp(comando, "LIBERA")==0){
             char nome[2];
             scanf("%s", nome);
             Lista *lista = encontra_listas(listas, quantidade_listas, nome[0]);
@@ -134,7 +134,77 @@ int main() {
                 exit(2);
             }
             libera(lista);
-        } else {
+        } 
+
+        else if(strcmp(comando, "PROD")==0){
+            // Escaneando nomes das listas a serem multiplicadas
+            char nome1[2], nome2[2];
+            scanf("%s %s", nome1, nome2);
+
+            // Encontrando listas, na lista de listas
+            Lista *lista1 = encontra_listas(listas, quantidade_listas, nome1[0]);
+            Lista *lista2 = encontra_listas(listas, quantidade_listas, nome2[0]);
+
+            // Caso algumas delas não exista o programa não faz nada
+            if( lista1 == NULL && lista2 = NULL){
+                printf("Alguma das listas não existe");
+                break;
+            }
+
+            // Caso ambas as litas existam chama a função multiplicação
+            else{
+                // Escaneia nome que será dado a lista 
+                char nome3[2];
+                scanf("%s", nome3);
+
+                // Chamda da função
+                Lista *lista = prod(lista1, lista2, nome3[0]);
+            }
+        }
+
+        else if(strcmp(comando, "IMPRIME")==0){
+            // Escaneando lista a ser impressa
+            char nome[2];
+            scanf("%s", nome);
+
+            // Buscando a lista
+            Lista *lista = encontra_listas(listas, quantidade_listas, nome);
+
+            // Verificação 
+            if ( lista == NULL){
+                printf("Lista não existe");
+                break;
+            }
+
+            // Chamando a função
+            else{
+                imprime(lista);
+            }
+           
+        }
+
+        else if(strcmp(comando, "IMPRIMEINV")==0){
+               // Escaneando lista a ser impressa
+            char nome[2];
+            scanf("%s", nome);
+
+            // Buscando a lista
+            Lista *lista = encontra_listas(listas, quantidade_listas, nome);
+
+            // Verificação 
+            if ( lista == NULL){
+                printf("Lista não existe");
+                break;
+            }
+
+            // Chamando a função
+            else{
+                imprime_inv(lista);
+            }
+           
+        }
+        
+        else {
             exit(3);
         }
     }
